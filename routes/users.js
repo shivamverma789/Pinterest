213b984ffbaabbdd6774b1a68a4c8f411f0c711a
shivamverma789 <shivamjitendraverma@gmail.com>
2024-03-15 13:57:49 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const plm =require('passport-local-mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/pinterest");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+   
+  },
+  posts: [
+    {
+      type:mongoose.Schema.Types.ObjectId,
+      ref: 'Post'
+    }
+  ],
+  dp: {
+    type: String // Assuming the dp (display picture) is stored as a URL or path
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  fullname: {
+    type: String,
+    require: true
+  }
+});
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model('User', userSchema);
+
+
